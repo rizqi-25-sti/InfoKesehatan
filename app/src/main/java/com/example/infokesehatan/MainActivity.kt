ec5b9cc722e028f40abf5eaa8789d6707ea7e43d
rizqi-25-sti <rizqi25520003.ac.id>
2026-05-19 14:58:19 +0700

    Initial commit

--- app/src/main/java/com/example/infokesehatan/MainActivity.kt
+++ app/src/main/java/com/example/infokesehatan/MainActivity.kt
@@ -7,7 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,6 +20,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -44,6 +53,32 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+
+private data class DrawableStringPair(
+    @DrawableRes val drawable: Int,
+    @StringRes val text: Int
+)
+
+private val alignYourBodyData = listOf(
+    R.drawable.ab1_inversions to R.string.ab1_inversions,
+    R.drawable.ab2_quick_yoga to R.string.ab2_quick_yoga,
+    R.drawable.ab3_stretching to R.string.ab3_stretching,
+    R.drawable.ab4_tabata to R.string.ab4_tabata,
+    R.drawable.ab5_hiit to R.string.ab5_hiit,
+    R.drawable.ab6_pre_natal_yoga to R.string.ab6_pre_natal_yoga,
+).map { DrawableStringPair(it.first, it.second) }
+
+private val favoriteCollectionsData = listOf(
+    R.drawable.fc1_short_mantras to R.string.fc1_short_mantras,
+    R.drawable.fc2_nature_meditations to R.string.fc2_nature_meditations,
+    R.drawable.fc3_stress_and_anxiety to R.string.fc3_stress_and_anxiety,
+    R.drawable.fc4_self_massage to R.string.fc4_self_massage,
+    R.drawable.fc5_overwhelmed to R.string.fc5_overwhelmed,
+    R.drawable.fc6_nightly_wind_down to R.string.fc6_nightly_wind_down,
+).map { DrawableStringPair(it.first, it.second) }
+
+
 
 @Composable
 fun SearchBar(
@@ -104,6 +139,24 @@ fun AlignYourBodyElement (
 }
 
 
+@Composable
+fun AlignYourBodyRow (
+    modifier: Modifier = Modifier
+) {
+    LazyRow (
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items (alignYourBodyData) { item ->
+            AlignYourBodyElement(
+                drawable = item.drawable,
+                text = item.text
+            )
+        }
+    }
+}
+
 
 @Composable
 fun FavoriteCollectionCard(
@@ -138,6 +191,48 @@ fun FavoriteCollectionCard(
 
 
 @Composable
+fun FavoriteCollectionsGrid (
+    modifier: Modifier = Modifier
+) {
+    LazyHorizontalGrid (
+        rows = GridCells.Fixed(2),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier.height(160.dp)
+    ) {
+        items(favoriteCollectionsData) { item ->
+            FavoriteCollectionCard(
+                drawable = item.drawable,
+                text = item.text,
+                modifier = Modifier.height(80.dp)
+            )
+        }
+    }
+}
+
+
+@Composable
+fun HomeSection (
+    @StringRes title: Int,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Column (modifier) {
+        Text (
+            text = stringResource(title),
+            style = MaterialTheme.typography.labelMedium,
+            modifier = Modifier.paddingFromBaseline(40.dp, 16.dp)
+                .padding(horizontal = 16.dp)
+        )
+        content()
+    }
+}
+
+
+
+
+@Composable
 fun HomeScreen() {
     Column(modifier = Modifier) {
         SearchBar()
@@ -153,3 +248,6 @@ fun HomeScreen() {
         )
     }
 }
+
+
+
